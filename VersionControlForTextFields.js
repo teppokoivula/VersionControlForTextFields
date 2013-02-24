3a5843d3a3a234c2ad7065cde931938a10be818c
teppokoivula <teppo@piikkilanka.com>
2013-02-24 19:26:14 +0200

    Improve support for different admin themes

--- VersionControlForTextFields.js
+++ VersionControlForTextFields.js
@@ -1,20 +1,19 @@
 $(function() {
 
-      var $pages_id = $('#PageIDIndicator').text();
+      var pages_id = $('#PageIDIndicator').text();
       var if_url = '../../setup/revision-history-for-text-fields/';
-      $.get(if_url, {pages_id: $pages_id}, function(data) {
+      $.get(if_url, {pages_id: pages_id}, function(data) {
 
           $('body').prepend(data);
 
           $('#text-field-history > div').each(function() {
-              var $field = $(this).attr('data-field');
-              var $header = $('.ui-widget-header[for=Inputfield_'+$field+']');
-              $header.append($(this)).addClass('with-history');
-              $(this).find('li:odd').addClass('parity-odd');
+              $('.ui-widget-header[for=Inputfield_'+$(this).attr('data-field')+']')
+                  .addClass('with-history')
+                  .after($(this));
+              $(this).find('a:first').addClass('ui-state-active');
           });
 
           $('.ui-widget-header.with-history').each(function() {
-              $(this).find('.field-revision-toggle:first').addClass('active');
               var $revisions_toggle = '<a class="field-revisions-toggle"><span class="ui-icon ui-icon-clock"></span></a>';
               if ($(this).find('.ui-icon').length) {
                   $(this).find('.ui-icon').after($revisions_toggle);
@@ -23,14 +22,14 @@ $(function() {
               }
           });
 
-          $('.field-revision-toggle').bind('click', function() {
-              $this = $(this);
-              if ($this.hasClass('active')) return false;
-              $this.parents('li.Inputfield:first').find('.field-revision-toggle.active').removeClass('active');
-              $this.addClass('active');
+          $('.field-revisions a').bind('click', function() {
+              if ($(this).hasClass('ui-state-active')) return false;
+              var $this = $(this);
               var $field = $this.attr('data-field');
               var $row_id = $this.attr('data-row-id');
-              $this.parents('li.Inputfield:first').find('.ui-widget-content').css('opacity', '0.25');
+              $this.parents('li.Inputfield:first').find('.field-revisions .ui-state-active').removeClass('ui-state-active');
+              $this.addClass('ui-state-active');
+              $this.parents('li.Inputfield:first').find('div.ui-widget-content').css('opacity', '0.25');
               $.get(if_url+'get', {id: $row_id}, function(data) {
                   if (tinyMCE && tinyMCE.get('Inputfield_'+$field)) {
                       tinyMCE.get('Inputfield_'+$field).setContent(data);
@@ -39,7 +38,7 @@ $(function() {
                   } else {
                       $this.parents('li.Inputfield:first').find('input[type=text]').val(data);
                   }
-                  $this.parents('li.Inputfield:first').find('.ui-widget-content').animate({
+                  $this.parents('li.Inputfield:first').find('div.ui-widget-content').animate({
                       opacity: 1
                   }, 500);
               });
@@ -47,21 +46,22 @@ $(function() {
           });
 
           $('.field-revisions-toggle').bind('click mouseenter', function() {
-              $revisions = $(this).siblings('.field-revisions');
+              $revisions = $(this).parent('label').siblings('.field-revisions');
               var show = ($revisions.is(':visible')) ? false : true;
               $('.field-revisions').slideUp();
               if (show) $revisions.slideDown();
               return false;
           });
 
+          var list_max_height = parseInt($('.field-revisions:first > ul').css('max-height'));
           $('.field-revisions')
               .bind('mouseleave', function() {
                   $(this).slideUp();
               })
               .each(function() {
-                  if ($(this).height() > 350) {
+                  if ($(this).height() > list_max_height) {
                       $(this).children().css('padding-right', '26px');
-                  } else console.log($(this).height());
+                  }
               });
 
       });
